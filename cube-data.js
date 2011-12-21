fe58060ff993e9ebba5f4597d089e7f22626fe20
petrosagg <petros@kamibu.com>
2011-12-21 17:56:02 +0200

    More demos

--- cube-data.js
+++ cube-data.js
@@ -65,4 +65,25 @@ var cubeTriangles = [
         [  1, 1, -1 ],
         [ -1, 1,  1 ]
     ],
-]
+];
+
+var colors = [ 
+    'aqua',
+    'blue',
+    'fuchsia',
+    'green',
+    'maroon',
+    'navy',
+    'purple',
+    'red',
+    'silver',
+    'teal',
+    'white',
+    'yellow'
+];
+
+var numTriangles = cubeTriangles.length;
+for ( var i = 0; i < numTriangles; i += 2 ) {
+    cubeTriangles[ i ].color =  colors[ ( i / 2 )  % colors.length ];
+    cubeTriangles[ i + 1 ].color = colors[ ( i / 2 )  % colors.length ];
+};
